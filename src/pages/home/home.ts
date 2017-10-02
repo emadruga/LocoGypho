87e34077d04b90cefaf37d8f785c0a3bb0ad0aec
Ewerton Madruga <emadruga@gmail.com>
2017-10-02 16:26:26 -0300

    Second commit: all initial plugins installed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,155 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, ModalController, Platform } from 'ionic-angular';
+import { Keyboard } from '@ionic-native/keyboard';
+import { DataProvider } from '../../providers/data/data';
+import { GiphyProvider } from '../../providers/giphy/giphy';
+import { FormControl } from '@angular/forms';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    giphyQueryValue: string;  
+    giphyQueryControl: FormControl;
 
-  }
+    constructor(public dataService: DataProvider, public giphyService: GiphyProvider, 
+		public modalCtrl: ModalController, public platform: Platform, 
+		public keyboard: Keyboard, public iab: InAppBrowser) {
+
+	this.giphyQueryControl = new FormControl();
+
+    }
+
+    ionViewDidLoad(){
+
+	this.giphyQueryControl.valueChanges.debounceTime(1500)
+	    .distinctUntilChanged().subscribe(giphyQuery => {
+
+		if(giphyQuery != '' && giphyQuery){
+		    this.giphyService.query = giphyQuery;
+		    this.changeGiphyQuery();
+		    this.keyboard.close();
+		}
+		
+	    });
+
+
+	this.platform.ready().then(() => {
+
+	    this.loadSettings();
+	    
+	});
+	
+    }
+
+    loadSettings(): void {
+	this.dataService.getData().then((settings) => {
+
+	    if(settings && typeof(settings) != "undefined"){
+
+		let newSettings = JSON.parse(settings);
+		this.giphyService.settings = newSettings;
+
+		if(newSettings.length != 0){
+		    this.giphyService.sort = newSettings.sort;
+		    this.giphyService.perPage = newSettings.perPage;
+		    this.giphyService.query = newSettings.giphyQuery;
+		}   
+
+	    }
+
+	    this.changeGiphyQuery();
+
+	});
+
+    }
+
+    showComments(post): void {
+	console.log("TODO:  showComments()");
+	let browser = this.iab.create('http://giphy.com' + post.data.permalink, '_system');
+    }
+
+    openSettings(): void {
+
+	let settingsModal = this.modalCtrl.create('SettingsPage', {
+	    perPage: this.giphyService.perPage,
+	    sort: this.giphyService.sort,
+	    giphyQuery: this.giphyService.query
+	});
+	
+	settingsModal.onDidDismiss(settings => {
+
+	    if(settings){
+		this.giphyService.perPage = settings.perPage;
+		this.giphyService.sort = settings.sort;
+		this.giphyService.query = settings.giphyQuery;
+
+		this.dataService.save(settings); 
+		this.changeGiphyQuery();      
+	    }
+
+	});
+
+	settingsModal.present();
+
+    }
+
+    playVideo(e, post): void {
+	console.log("TODO: playVideo()");
+
+	//Create a reference to the video
+	let video = e.target;
+
+	if(!post.alreadyLoaded){
+	    post.showLoader = true;     
+	}
+
+	//Toggle the video playing
+	if(video.paused){
+
+	    //Show the loader gif
+	    video.play();
+
+	    //Once the video starts playing, remove the loader gif
+	    video.addEventListener("playing", (e) => {
+		post.showLoader = false;
+		post.alreadyLoaded = true;
+	    });
+
+	} else {
+	    video.pause();
+	}
+	
+
+
+    }
+
+    shuffleGiphyQuery(): void {
+
+	var favorites = ["mechanical_gifs", "HorrorGifs", "gifextra", "TrippyGIFs"];
+	var giphyQuery = favorites[Math.floor(Math.random() * favorites.length)];
+	var msg = "GiphyQuery is " + giphyQuery;
+	console.log(msg);
+	this.giphyService.query = giphyQuery;
+	this.changeGiphyQuery();
+    }
+
+    changeGiphyQuery(): void {
+	console.log("TODO: changeGiphyQuery()");
+	this.giphyService.resetPosts();
+    }
+
+    loadMore(): void {
+	console.log("TODO: loadMore()");
+	this.giphyService.nextPage();
+    }
 
 }
+
---
+++ src/providers/giphy/giphy.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GiphyProvider {
+
+    settings: any;
+    loading: boolean = false;
+    posts: any = [];
+    query: string = 'happy';
+    page: number = 1;
+    perPage: number = 15;
+    myAPI_key = 'mX5hQKOb0eqFCJtCKY4I0SSAGPsceoXV'
+    offset: number = 0;
+    stopIndex: number;
+    sort: string = 'hot';
+    responseCount: number = 0;
+    count: number = 0; 
+    moreCount: number = 0;
+
+    constructor(public http: Http) {
+
+    }
+
+    fetchData(): void {
+
+	let query =  this.query.replace(/\s+/g,'+');
+
+	//Build the URL that will be used to access the API based on the users current preferences
+	let url = 'https://api.giphy.com/v1/gifs/search?api_key=' + this.myAPI_key + '&q=' + query + '&limit='
+	    + this.perPage + '&offset=' + this.offset + '&rating=G&lang=en'
+
+	console.log('URL is: ' + url);
+
+	//We are now currently fetching data, so set the loading variable to true
+	this.loading = true;
+
+	//Make a Http request to the URL and subscribe to the response
+	this.http.get(url).map(res => res.json()).subscribe(data => {
+
+	    let stopIndex = this.posts.length;
+	    this.posts = this.posts.concat(data.data);
+	    this.responseCount = data.pagination.total_count;
+	    this.count = data.pagination.count;
+
+	    console.log(data);
+
+	    //Loop through all NEW posts that have been added. 
+	    for(let i = this.posts.length - 1; i >= stopIndex; i--){
+
+		let post = this.posts[i];
+
+		//Add a new property that will later be used to toggle a loading animation
+		//for individual posts
+		post.showLoader = false;
+		post.alreadyLoaded = false;
+
+		// console.log(post);
+		// if ('mp4' in post.images.looping) {
+		//     console.log("Found mp4");
+		// } else {
+		//     console.log("Not found mp4");
+		// }
+		    
+		this.posts[i].url = post.images.looping.mp4;
+		this.posts[i].snapshot = post.images["480w_still"].url;
+
+		if( 'source_tld' in post && post.source_tld != ""){
+		    this.posts[i].title    = post.source_tld;
+		} else {
+		    this.posts[i].title    = post.id;
+		}
+		this.loading = false;
+
+	    }
+
+
+	}, (err) => {
+	    //Fail silently, in this case the loading spinner will just continue to display
+	    console.log("query doesn't exist!");
+	});
+    }
+
+    nextPage(){
+
+	if(this.responseCount < (this.perPage * (this.page+1) ) || this.moreCount > 3){
+
+	    this.moreCount = 0;
+	    this.loading = false;
+	    this.resetPosts();
+
+	} else {
+	    this.page++;
+	    this.offset += this.count;
+	    this.fetchData();
+	    this.moreCount++;
+	} 
+
+    }
+
+    resetPosts(){
+	this.page = 1;
+	this.posts = [];
+	this.offset = 0;
+	this.responseCount = 0;
+	this.count = 0;
+	this.fetchData();
+    }
+
+}
+
